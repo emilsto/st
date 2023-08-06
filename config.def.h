374248d564aaba151e5b24719e1d62cf39c1b41b
Emil <emil.kantaneva@gmail.com>
2023-08-06 19:45:57 +0300

    new colorscheme

--- config.def.h
+++ config.def.h
@@ -93,48 +93,44 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.9;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+  /* 8 normal colors */
+  [0] = "#292929", /* black   */
+  [1] = "#cf6a4c", /* red     */
+  [2] = "#19cb00", /* green   */
+  [3] = "#fad07a", /* yellow  */
+  [4] = "#8197bf", /* blue    */
+  [5] = "#8787af", /* magenta */
+  [6] = "#668799", /* cyan    */
+  [7] = "#888888", /* white   */
 
-	[255] = 0,
+  /* 8 bright colors */
+  [8]  = "#525252", /* black   */
+  [9]  = "#ff9d80", /* red     */
+  [10] = "#23fd00", /* green   */
+  [11] = "#ffefbf", /* yellow  */
+  [12] = "#accaff", /* blue    */
+  [13] = "#c4c4ff", /* magenta */
+  [14] = "#80bfaf", /* cyan    */
+  [15] = "#e8e8d3", /* white   */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+  /* special colors */
+  [256] = "#151515", /* background */
+  [257] = "#888888", /* foreground */
 };
-
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * foreground, background, cursor
  */
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+unsigned int defaultcs = 257;
+unsigned int defaultrcs = 256;
 
 /*
  * Default shape of cursor
